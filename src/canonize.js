c53a6af2b09d3a1cca2266e08dc1baa288309f81
Shpironok <shpironoka@gmail.com>
2016-11-10 13:51:08 +0300

    task 2C

--- src/canonize.js
+++ src/canonize.js
@@ -1,5 +1,5 @@
 export default function canonize(url) {
-	const re = new RegExp('@?(https?:)?\/?\/?([a-zA-Z0-9\.\-]*\/)?([a-zA-Z0-9\.\@\_]*)', 'i');
+	const re = new RegExp('@?(https?:)?\/?\/?([a-zA-Z0-9\.\-]*\/)?([a-zA-Z0-9\.\@\_]*)');
 	const username = url.match(re);
 
 	return username;
