fee71e494eb23cbb6ab2ec217ef169e9313d9262
Shpironok <shpironoka@gmail.com>
2016-11-14 00:12:25 +0300

    task 3A

--- src/index.js
+++ src/index.js
@@ -1,62 +1,58 @@
 import express from 'express';
+import fetch from 'isomorphic-fetch';
+import Promise from 'bluebird';
+import _ from 'lodash';
 import cors from 'cors';
-import canonize from './canonize';
+
+var pc;
+getPc();
 
 const app = express();
 app.use(cors());
-app.get('/task2B', (req, res) => {
-  const username = canonize(req.query.fullname);
-      username[3] = registerCheck(username[3]);
-      username[2] = firstLetter(username[2]);
-      username[1] = firstLetter(username[1]);
-      var shortname = `${username[3]} ${username[1]} ${username[2]}`;
-  res.send(shortname);
+
+app.get(/.+/, (req,res) => {
+    let path = req.path.replace(/^\//,'').replace(/\/$/,'').split('/');
+    if (path[0]=='volumes') res.json(getVolumes());
+    else if (path[0]=='') res.json(pc);
+    else sendComponent(path,res);
 });
 
-app.get('/task2A', (req, res) => {
-	const sum = (+req.query.a || 0) + (+req.query.b || 0);
-	res.send(sum.toString());
-});
+app.listen(3000);
 
-app.listen(3000, () => {
-  //console.log('Your app listening on port 3000!');
-});
-
-const names = [
-     'Vladimir/Vladimirovich/Putin',
-	  'Tinna Gunnlaugsdottir',
-	  'Steave Jobes',
-	  'Steave Paul Jobes',
-	  'Василий Иванович Чапаев',
-	  'Vladimir27 Vladimirovich Putin',
-	  'Vladimir V. Putin',
-	  'иГоРь аЛексАндРовиЧ сУвороВ',
-	  'Ваня Грозный',
-	  'Billy',
-	  'Сто сорок четыре абизяны'
-];
-
-
-function registerCheck(badName) {
-  const normalName = badName.charAt(0).toUpperCase() + badName.slice(1).toLowerCase();
-
-  return normalName;
+function sendComponent(path, res) {
+    let component = pc;
+    try {
+        path.forEach(el => {
+            if (_.isArray(component)) {
+                if(!_.isNaN(+el) && component.hasOwnProperty(el)) component=component[el];
+                else throw new Error('Not Found');
+            }
+            else if (_.isString(component)) throw new Error('Not Found');
+            else if (component.hasOwnProperty(el)) component=component[el];
+            else throw new Error('Not found');
+        });
+        res.json(component);
+    }
+    catch(e) {
+        res.sendStatus(404);
+    }
 }
 
-function firstLetter(badName) {
-  const normalName = badName.charAt(0).toUpperCase() + '.';
-
-  return normalName;
+function getVolumes() {
+    const volumes={};
+    var size;
+    pc.hdd.forEach(el => {
+        if(volumes[el.volume]) volumes[el.volume]+=el.size;
+        else volumes[el.volume]=el.size;
+    });
+    for(size in volumes) volumes[size]+='B';
+    //console.log(volumes);
+    return volumes;
 }
 
-names.forEach((fullName) => {
-	const username = canonize(fullName);
-      username[3] = registerCheck(username[3]);
-      username[2] = firstLetter(username[2]);
-      username[1] = firstLetter(username[1]);
-
-	console.log(username[3] + ' ' + username[1] + ' ' + username[2]);
-	//console.log(username);
-	//console.log(username);
-})
-
+async function getPc() {
+    const pcUrl='https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+    return await fetch(pcUrl)
+    	.then(async (res) => pc = await res.json())
+		.catch(e => console.log(e));
+}
